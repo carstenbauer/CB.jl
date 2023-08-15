db17c9d564c415606b3009c7f837af200581c6a3
Carsten Bauer <crstnbr@gmail.com>
2023-08-15 10:51:48 +0200

    Update startup.jl

--- startup.jl
+++ startup.jl
@@ -8,7 +8,7 @@ atreplinit() do repl
     end
 
     try
-        using Revise
+        @eval using Revise
     catch e
         @warn "Error initializing Revise" exception=(e, catch_backtrace())
     end
